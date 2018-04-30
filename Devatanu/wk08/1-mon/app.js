575ed8d2fee58b6cfd7176cb01eff2cfbe0772e3
Devatanu Deka <deva.deka@gmail.com>
2018-04-30 17:04:46 +1000

    commit 7 & 8

--- Devatanu/wk08/1-mon/app.js
+++ Devatanu/wk08/1-mon/app.js
@@ -1,13 +1,51 @@
 var $setColourBtn = $('.set-colour-btn');
 var $setColourTxt = $('.set-colour-txt');
+var $setBackgroundBtn = $('.set-bg-btn');
+var $setBackgroundTxt = $('.set-bg-txt');
 var $brushDiv = $('.brush');
 var $containerDiv = $('.container');
-
+var $bgSelect = $('.bg-select');
+var $res = null;
 $setColourBtn.on('click', function(event){
     event.preventDefault();
     $brushDiv.css("background-color", $setColourTxt.val());
 });
 
+
+var API_KEY = "2f6435d9";
+
+$setBackgroundBtn.on('click', function(event){
+    event.preventDefault();
+
+    var options = {
+        url: `http://www.omdbapi.com/?apikey=${API_KEY}&s=${$setBackgroundTxt.val()}`
+      }
+
+    
+    $.ajax(options).done(function(result){
+        
+        if(result.Search.length > 0){
+            $bgSelect.empty();
+            $bgSelect.append($('<option>', {
+                value: "none",
+                text: 'Select a movie'
+            }));
+            result.Search.forEach(movie => {
+                $bgSelect.append($('<option>', {
+                    value: movie.Poster,
+                    text: movie.Title
+                }));
+            });
+        }
+    });
+
+    $brushDiv.css("background-color", $setColourTxt.val());
+});
+
+$bgSelect.on('change', function() {
+    $containerDiv.css('background-image',`url("${this.value}")`);
+    console.log(this.value);
+  });
 
 $( document ).ready(function() {
     for(var i = 0; i<100; i++){
@@ -17,6 +55,8 @@ $( document ).ready(function() {
 
     $setColourTxt.val("mistyrose");
     $brushDiv.css('background-color',$setColourTxt.val());
+
+
 
 });
 
